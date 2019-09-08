e706e3fc7a26f6e94af6896c16daab51f02c3bec
Вячеслав <mirsonov.slava@mail.ru>
2019-09-08 14:13:23 +0300

    Добавьте файлы проекта.

---
+++ ПроектПробный/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПроектПробный
+{
+	class Program
+	{
+		const int a = 10;
+		static void Main(string[] args)
+		{
+			for (int i = 0;i<a;i++)
+			{
+				Console.WriteLine("Русские вперед!!!");
+			}
+			Console.ReadLine();
+		}
+	}
+}
